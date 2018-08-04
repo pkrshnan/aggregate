01753c345f112f3f96acad471db9df5467608216
Pranav Krishnan <pranavkrsh@gmail.com>
2018-08-04 01:17:01 -0400

    Heroku New Server File

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ var path = require('path');
 var serveStatic = require('serve-static');
 
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(path.join(__dirname, 'dist')));
 
 var port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log('server started '+ port);
